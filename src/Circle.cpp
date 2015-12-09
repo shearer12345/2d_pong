22143db0369d29be4e2f5076f0d43a7e5a50172e
shearer12345 <shearer12345@gmail.com>
2015-12-09 07:41:55 +0000

    fix glm::two_pi issue - include correct constants file, and use appropriately

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -1,11 +1,12 @@
 #include "Circle.hpp"
-#include <glm/gtx/constants.hpp>
+#include <glm/gtc/constants.hpp>
 
 Circle::Circle(GLSLProgram* glslProgram, int segments) :
               Mesh::Mesh(glslProgram),
               segments(segments)
 {
-    GLfloat anglePerSegmentRads = 3.141 * 2.0f / (float)segments;
+    const static float TWO_PI = glm::two_pi<float>();
+    GLfloat anglePerSegmentRads = TWO_PI / (float)segments;
 
     //top
     vertexData.push_back(Vertex(   glm::vec3( 0.0f, 1.0f , 0.0f),  glm::vec4(1.0f, 1.0f, 1.0f,  1.0f), glm::vec2(0.5f, 1.0f)  ));
